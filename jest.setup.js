e43edd6e51e36821807af60a8fcaaff73d195974
YasiruNil <yasirudam@gmail.com>
2026-05-31 13:44:31 +0530

    add: test workflow

---
+++ jest.setup.js
@@ -0,0 +1,9 @@
+const { TextEncoder, TextDecoder } = require('util')
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder
+
+global.ResizeObserver = class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
---
+++ src/tests/App.test.jsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+
+describe('Navbar', () => {
+  it('renders the brand name', () => {
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    )
+    expect(screen.getByText(/YASIRU N\./i)).toBeTruthy()
+  })
+
+  it('renders all nav links', () => {
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    )
+    expect(screen.getByText('Home')).toBeTruthy()
+    expect(screen.getByText('Experience')).toBeTruthy()
+    expect(screen.getByText('Projects')).toBeTruthy()
+    expect(screen.getByText('Contact')).toBeTruthy()
+  })
+
+  it('renders the Hire Me button', () => {
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    )
+    expect(screen.getByRole('button', { name: /hire me/i })).toBeTruthy()
+  })
+})
